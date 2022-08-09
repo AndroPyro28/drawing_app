ad7293158a52cdfb8d7f181aba8152d42f62dc83
andropyro28 <menandroeugenio1028@gmail.com>
2022-08-09 01:30:34 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import './style.css'
+
+window.addEventListener('load', _ => {
+  const canvas = document.querySelector('#canvas')! as HTMLCanvasElement;
+
+const ctx = canvas.getContext('2d')!;
+
+  //resizing
+
+  canvas.height = window.innerHeight;
+  canvas.width = 800;
+
+  // x, y, width, height strokeRectangle
+  // ctx.strokeStyle = 'blue'
+  // ctx.fillStyle = 'red'
+  // ctx.strokeRect(50, 50, 200, 200);
+
+  // line x and y
+
+  // ctx.beginPath();
+  // ctx.moveTo(100, 100);
+  // ctx.lineTo(200, 100);
+  // ctx.lineTo(200, 150);
+  // ctx.closePath()
+  // ctx.stroke()
+
+  let painting = false;
+
+  // event listener
+
+  const startPosition = (e: MouseEvent):void => {
+    ctx.beginPath()
+    painting = true;
+    draw(e);
+  }
+  
+  const finishPosition = ():void => {
+    painting = false;
+  }
+
+  const draw = (e: MouseEvent) => {
+    if(painting) {
+      ctx.lineWidth = 5;
+      ctx.lineCap = 'round';
+      const coordX = e.clientX
+      const coordY = e.clientY
+      ctx.lineTo(coordX,coordY);
+      ctx.stroke();
+      
+      ctx.moveTo(coordX, coordY)
+    }
+  }
+
+  canvas.addEventListener('mousedown', startPosition );
+  canvas.addEventListener('mouseup', finishPosition );
+  canvas.addEventListener('mousemove', draw);
+  const buttonPrint = document.querySelector('#putImageBtn')!;
+  const buttonClear = document.querySelector('#clearCanvasBtn')!;
+  const img = document.querySelector('#img')! as HTMLImageElement;
+
+  buttonPrint.addEventListener('click', () => {
+    // const imgData = ctx.getImageData(0,0, 1000, 1000);
+
+    const dataURL = canvas.toDataURL();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    img.src = dataURL;
+    // ctx.putImageData(dataURL, 100, 100);
+  })
+
+  buttonClear.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    img.src = ""
+  })
+})
