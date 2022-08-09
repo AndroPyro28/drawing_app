daa2aafbd68c28276c209a4bfdcc1bbd7cf5a3c1
andropyro28 <menandroeugenio1028@gmail.com>
2022-08-09 02:13:50 -0400

    updating mini drawing app

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import './style.css'
+import "./style.css";
 
-window.addEventListener('load', _ => {
-  const canvas = document.querySelector('#canvas')! as HTMLCanvasElement;
+window.addEventListener("load", (_) => {
+  const canvas = document.querySelector("#canvas")! as HTMLCanvasElement;
 
-const ctx = canvas.getContext('2d')!;
+  const ctx = canvas.getContext("2d")!;
 
   //resizing
 
   canvas.height = window.innerHeight;
   canvas.width = 800;
+  ctx.strokeStyle = 'yellow'
 
   // x, y, width, height strokeRectangle
   // ctx.strokeStyle = 'blue'
@@ -28,37 +29,65 @@ const ctx = canvas.getContext('2d')!;
 
   // event listener
 
-  const startPosition = (e: MouseEvent):void => {
-    ctx.beginPath()
+  const startPosition = (e: MouseEvent): void => {
+    ctx.beginPath();
     painting = true;
     draw(e);
-  }
-  
-  const finishPosition = ():void => {
+  };
+
+  const finishPosition = (): void => {
     painting = false;
-  }
+  };
 
   const draw = (e: MouseEvent) => {
-    if(painting) {
+    if (painting) {
+
       ctx.lineWidth = 5;
-      ctx.lineCap = 'round';
-      const coordX = e.clientX
-      const coordY = e.clientY
-      ctx.lineTo(coordX,coordY);
+      ctx.lineCap = "round";
+      const coordX = e.clientX;
+      const coordY = e.clientY;
+      ctx.lineTo(coordX, coordY);
       ctx.stroke();
-      
-      ctx.moveTo(coordX, coordY)
+
+      ctx.moveTo(coordX, coordY);
     }
-  }
+  };
 
-  canvas.addEventListener('mousedown', startPosition );
-  canvas.addEventListener('mouseup', finishPosition );
-  canvas.addEventListener('mousemove', draw);
-  const buttonPrint = document.querySelector('#putImageBtn')!;
-  const buttonClear = document.querySelector('#clearCanvasBtn')!;
-  const img = document.querySelector('#img')! as HTMLImageElement;
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishPosition);
+  canvas.addEventListener("mousemove", draw);
+  const buttonPrint = document.querySelector("#putImageBtn")!;
+  const buttonClear = document.querySelector("#clearCanvasBtn")!;
+  const img = document.querySelector("#img")! as HTMLImageElement;
 
-  buttonPrint.addEventListener('click', () => {
+  const btnBlue = document.querySelector("#btnBlue");
+  const btnRed = document.querySelector("#btnRed");
+  const btnGreen = document.querySelector("#btnGreen");
+  const btnYellow = document.querySelector("#btnYellow");
+  const btnWhite = document.querySelector("#btnWhite");
+  const btnBrown = document.querySelector("#btnBrown");
+
+  btnBlue?.addEventListener("click", () => {
+     ctx.strokeStyle = 'blue'
+  });
+  btnRed?.addEventListener("click", () => {
+     ctx.strokeStyle = 'red'
+  });
+  btnGreen?.addEventListener("click", () => {
+     ctx.strokeStyle = 'green'
+  });
+  btnYellow?.addEventListener("click", () => {
+     ctx.strokeStyle = 'yellow'
+  });
+
+  btnWhite?.addEventListener("click", () => {
+    ctx.strokeStyle = 'white'
+ });
+  btnBrown?.addEventListener("click", () => {
+     ctx.strokeStyle = 'brown'
+  });
+
+  buttonPrint.addEventListener("click", () => {
     // const imgData = ctx.getImageData(0,0, 1000, 1000);
 
     const dataURL = canvas.toDataURL();
@@ -66,10 +95,10 @@ const ctx = canvas.getContext('2d')!;
 
     img.src = dataURL;
     // ctx.putImageData(dataURL, 100, 100);
-  })
+  });
 
-  buttonClear.addEventListener('click', () => {
+  buttonClear.addEventListener("click", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    img.src = ""
-  })
-})
+    img.src = "";
+  });
+});
